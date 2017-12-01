db8113fd055b6ae8c7d9adb2adcce7f50af8fd95
Tom John <tom.john@rea-group.com>
2017-12-01 12:24:30 +1100

    Add runGame to take a hardcoded list of shot positions

--- GameBoard.hs
+++ GameBoard.hs
@@ -5,8 +5,8 @@ import Vessel
 
 data GameBoard = GameBoard Position Position [Vessel] deriving Show
 
-takeShot :: Position -> GameBoard -> GameBoard
-takeShot shot (GameBoard p1 p2 vessels) = (GameBoard p1 p2 (shootVessels shot vessels))
+takeShot :: GameBoard -> Position -> GameBoard
+takeShot (GameBoard p1 p2 vessels) shot = (GameBoard p1 p2 (shootVessels shot vessels))
   where shootVessels _ [] = []
         shootVessels p (v:vs) = if vesselContains p v then addHit p v : vs else v : shootVessels p vs
 
@@ -14,3 +14,10 @@ gameOver :: GameBoard -> Bool
 gameOver (GameBoard _ _ vessels) = allSunk vessels
   where allSunk [] = True
         allSunk (v:vs) = isSunk v && allSunk vs
+
+allBoardPositions :: GameBoard -> IO ()
+allBoardPositions (GameBoard _ _ vessels) = doIt vessels
+  where doIt [] = putStrLn ""
+        doIt (v:vs) = do
+          print v
+          doIt vs
--- Main.hs
+++ Main.hs
@@ -9,9 +9,15 @@ main :: IO ()
 main = do
   putStrLn "Generating board ..."
   let board = generateBoard
-  let newBoard = takeShot nextShot board
-  print (gameOver newBoard)
-  putStrLn "Game Over - You lose!"
+  let shots = [Position 0 2,Position 0 3,Position 0 4,Position 0 5,Position 0 6,
+               Position 4 1,Position 5 1,Position 6 1,Position 7 1,
+               Position 3 8,Position 4 8,Position 5 8,
+               Position 5 5,Position 6 5,Position 7 5,
+               Position 4 4,Position 4 5]
+  let finalBoard = runGame board shots
+  allBoardPositions finalBoard
+  putStrLn "Game Over!"
+  putStrLn ("All sunk = " ++ (show (gameOver finalBoard)))
 
 generateBoard :: GameBoard
 generateBoard = do
@@ -23,9 +29,8 @@ generateBoard = do
   let vessels = [carrier, battleship, cruiser, submarine, destroyer]
   GameBoard (Position 0 0) (Position 9 9) vessels
 
-nextShot :: Position
-nextShot = do
-  -- TODO get input from user
-  let nextX = 0
-  let nextY = 2
-  Position nextX nextY
+runGame :: GameBoard -> [Position] -> GameBoard
+runGame b [] = b
+runGame board (p:ps) = do
+  let newBoard = takeShot board p
+  runGame newBoard ps
