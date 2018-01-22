23b8709490d3330aea6cfa9279b76212ea396be5
Tom John <tom.john@rea-group.com>
2018-01-22 14:00:48 +1100

    Change board print functions to return String rather than IO ()

--- GameBoard.hs
+++ GameBoard.hs
@@ -30,7 +30,7 @@ generateBoard = do
   vs <- buildVessels vesselBuilders bL tR []
   pure $ GameBoard bL tR vs Set.empty
 
-buildVessels :: [(Orientation -> Pos -> Vessel)] -> Pos -> Pos -> [Vessel] -> IO [Vessel]
+buildVessels :: [Orientation -> Pos -> Vessel] -> Pos -> Pos -> [Vessel] -> IO [Vessel]
 buildVessels [] _ _ acc = pure acc
 buildVessels (f:fs) bL tR acc = do
   vessel <- buildVessel f bL tR
@@ -50,7 +50,7 @@ buildVessel f bL tR = do
 vesselOffBoard :: Pos -> Vessel -> Bool
 vesselOffBoard tR vessel = do
   let lastPos = last $ Set.elems $ positions vessel
-  (fst lastPos) > (fst tR) || (snd lastPos) > (snd tR)
+  (fst lastPos > fst tR) || (snd lastPos > snd tR)
 
 overlapsAny :: Vessel -> [Vessel] -> Bool
 overlapsAny _ [] = False
@@ -86,44 +86,41 @@ printBoard :: GameBoard -> IO ()
 printBoard (GameBoard (x1,y1) (x2,y2) vs ms) = do
   let xs = [x1..x2]
   let ys = reverse [y1..y2]
-  printRows (listHits vs) ms xs ys
+  let strs = printRows (listHits vs) ms xs ys
+  printStrings strs
   putStr "  ="
   putStrLn (concat $ replicate ((maximum xs - minimum xs) + 1) "====")
   putStr "   "
   _ <- mapM (\x -> putStr $ " " ++ show x ++ "  ") xs
   putStrLn ""
 
-printRows :: PosSet -> PosSet -> [Int] -> [Int] -> IO ()
-printRows _ _ _ [] = do
-  pure ()
+printStrings :: [String] -> IO ()
+printStrings [] = pure ()
+printStrings (s:ss) = do
+  putStrLn s
+  printStrings ss
+
+printRows :: PosSet -> PosSet -> [Int] -> [Int] -> [String]
+printRows _ _ _ [] = []
 printRows hs ms xs (y:ys) = do
-  putStrLn "  ========================================="
-  putStr $ (show y) ++ " "
-  printRow hs ms y xs
-  printRows hs ms xs ys
+  let rowHeader = "  ========================================="
+  let row = show y ++ " " ++ printRow hs ms y xs
+  rowHeader : (row : printRows hs ms xs ys)
 
-printRow :: PosSet -> PosSet -> Int -> [Int] -> IO ()
-printRow hs ms y xs = do
-  putStr "|"
-  printSquares hs ms y xs
-  putStrLn ""
+printRow :: PosSet -> PosSet -> Int -> [Int] -> String
+printRow hs ms y xs =
+  "|" ++ printSquares hs ms y xs
 
-printSquares :: PosSet -> PosSet -> Int -> [Int] -> IO ()
-printSquares _ _ _ [] = do
-  pure ()
-printSquares hs ms y (x:xs) = do
-  printSquare hs ms x y
-  printSquares hs ms y xs
+printSquares :: PosSet -> PosSet -> Int -> [Int] -> String
+printSquares _ _ _ [] = ""
+printSquares hs ms y (x:xs) =
+  printSquare hs ms x y ++ printSquares hs ms y xs
 
-printSquare :: PosSet -> PosSet -> Int -> Int -> IO ()
-printSquare hs ms x y = do
-  putStr " "
-  if elem (x,y) hs
-    then putStr "X"
-    else if elem (x,y) ms
-      then putStr "-"
-    else putStr " "
-  putStr " |"
+printSquare :: PosSet -> PosSet -> Int -> Int -> String
+printSquare hs ms x y
+  | (x,y) `elem` hs = " X |"
+  | (x,y) `elem` ms = " - |"
+  | otherwise = "   |"
 
 -- TODO try and get all IO into Main.hs!
 shoot :: GameBoard -> Pos -> IO GameBoard
@@ -131,30 +128,30 @@ shoot board shot = do
   result <- takeShot board shot
   case result of
     OffBoard -> do
-      putStrLn $ "Off board " ++ (show shot)
+      putStrLn $ "Off board " ++ show shot
       pure board
     RepeatShot -> do
-      putStrLn $ "Repeat shot " ++ (show shot)
+      putStrLn $ "Repeat shot " ++ show shot
       pure board
     Hit newVessels -> do
-      putStrLn $ "HIT " ++ (show shot)
+      putStrLn $ "HIT " ++ show shot
       pure $ board {vessels=newVessels}
     Miss -> do
-      putStrLn $ "MISS " ++ (show shot)
-      pure $ board {misses=(Set.insert shot (misses board))}
+      putStrLn $ "MISS " ++ show shot
+      pure $ board {misses=Set.insert shot (misses board)}
 
 takeShot :: GameBoard -> Pos -> IO ShotResult
-takeShot (GameBoard bL tR vs ms) shot = do
+takeShot (GameBoard bL tR vs ms) shot =
   if not $ onBoard bL tR shot
     then pure OffBoard
-  else if elem shot ms
+  else if shot `elem` ms
     then pure RepeatShot
   else
     doIt vs shot []
   -- TODO could try runWriter here to build a pair of (ShotResult, [Vessel]) (avoids the need for doIt accumulator and all the list concatenation)
   where doIt [] _ _ = pure Miss
-        doIt (v:vRest) s acc = do
-          if elem s (hits v)
+        doIt (v:vRest) s acc =
+          if s `elem` hits v
             then pure RepeatShot
           else if isHit s v
             then do
